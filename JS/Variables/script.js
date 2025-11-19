26eb604e86faff9041ac521e0e1141228357f78a
Sana Ullah Siddiqui <156604999+SanaUllahCoder@users.noreply.github.com>
2025-11-19 12:50:43 +0000

    learn variables

---
+++ JS/Variables/script.js
@@ -0,0 +1,24 @@
+
+var myName; // It's is camel case
+var MyName; // It's is pascal case
+var myname; // It's is kebab case
+
+var name1 = 'Saylani';
+var num1 = 12;
+var num2 = 34;
+var num3 = "11";
+var num4 = "34";
+var greeting = 'assalam o alikum';
+
+// document.write(greeting  + name1); //without space
+document.write(greeting + ' ' + name1+ "<br>");
+document.write(name1 + "<br>");
+document.write(num1 + "<br>");
+document.write(num1 + num3 + "<br>");
+document.write(num1 + num4 + "<br>");
+document.write(num3 - num1 + "<br>");
+document.write(num3 * num2 + "<br>");
+document.write(num2 / num4 + "<br>");
+
+
+
