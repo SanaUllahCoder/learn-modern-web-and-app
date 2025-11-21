fefb99e14473935b4a503e3ee7b547922a007dfe
Sana Ullah Siddiqui <156604999+SanaUllahCoder@users.noreply.github.com>
2025-11-21 12:51:25 +0000

    learn operators and etc.

---
+++ JS/Variables/class 2/script.js
@@ -0,0 +1,32 @@
+
+var myName;
+
+var firstName = 'Sana Ullah';
+var lastName = 'Siddiqui';
+
+var num1 = 1;
+var num2 = 2;
+
+
+var fullName = document.write(firstName + ' ' + lastName); // it's is concate function
+console.log(fullName)
+console.log(num1 + num2)
+
+
+// var student1 = alert();
+// var student2 = alert();
+
+var modulus = 3%4;
+document.write(modulus)
+
+var increment = 10;
+increment++;
+document.write(increment);
+
+var decrement = 10;
+decrement--;
+document.write(decrement);
+
+var num = 1;
+var newNum = ++num;
+document.write(num);
