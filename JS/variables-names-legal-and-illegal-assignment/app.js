947f44a7261c0124c6613b7d3204b989cf492724
Sana Ullah Siddiqui <156604999+SanaUllahCoder@users.noreply.github.com>
2025-12-01 14:04:05 +0500

    complete assignment variables names legal and illegal

---
+++ JS/variables-names-legal-and-illegal-assignment/app.js
@@ -0,0 +1,60 @@
+// Q1: Type the illegal character in this statement.
+// var product cost = 3.45;
+// ans:  I wanted you to type a space. A space is an illegal character in a variable name.
+
+// Q2: Rewrite this using camelCase.
+// var Nameofband;
+
+// Q3: Declare a variable using nothing but all of the legal characters that aren't alphabet characters or numbers. Don't repeat any of them.
+// ans: var $_;  or   var _$;
+
+// Q4: Holding down the left mouse button, drag the pieces of code onto the screen to form a statement that declares a variable in camelCase.
+// ans: var firstName;
+
+// Q5: In a single statement, declare the variable firstLegalChar and assign it a string. The string consists entirely of the first character on the keyboard in the top row that's legal in a variable name, as long as the character doesn't come first.
+// ans: var firstLegalChar = "1";
+
+// Q6: Declare a variable, in camelCase, that combines the two syllables boo and hoo.
+// ans: var booHoo;
+
+// Q7: In a single statement declare a legally-named variable and assign a number to it.
+// ans: var topScore = 100;
+
+// Q8: In a single statement declare a legally-named variable and assign to it the sum of 2 other legally-named variables.
+// ans: var total = cost + profit;
+
+// Q9: Declare a variable whose name is the combination of 3 characters, in this order: a legal first character that is not a letter, a character that would be illegal as a first character, and a character that would be legal anywhere in the name.
+// ans: var _9w;
+
+// Q10: The variable boogieWoogie has already been declared. Assign to it the value of another variable, woogieBoogie.
+// ans: var boogieWoogie = woogieBoogie;
+
+// Q11: Declare a variable that is a combination of your first and last names. Use camelCase.
+// ans: var sanaullahSiddiqui;
+
+// Q12: Declare a variable using nothing but all of the legal characters that aren't alphabet characters or numbers. Don't repeat any of them. (The timer starts when you click in the answer field.)
+// ans: var $_; or var _$;
+
+// Q13: In a single statement declare a legally-named variable and assign a number to it.
+// ans: var num = 3;
+
+// Q14: In a single statement declare a legally-named variable and assign to it the sum of 2 other legally-named variables.
+// ans: var total = cost + profit;
+
+// Q15: Declare a variable whose name is the combination of 3 characters, in this order: a legal first character that is not a letter, a character that would be illegal as a first character, and a character that would be legal anywhere in the name.
+// ans: var _9w;
+
+// Q16  The variable boogieWoogie has already been declared. Assign to it the value of another variable, woogieBoogie.
+// ans: boogieWoogie = WoogieBoogie
+
+// Q17: Declare a variable that is a combination of your first and last names. Use camelCase. If you have a complex name, simplify it. Use "Sarah Billings" instead of "Sarah Sue Billings-Coopersmith III"
+// ans: var markMyers;
+
+
+
+
+
+
+
+
+
