122f5d1e083c4d0d214efb0c304c4568df6d55ea
Sana Ullah Siddiqui <156604999+SanaUllahCoder@users.noreply.github.com>
2025-12-01 13:13:00 +0500

    complete assignment variable for numbers

---
+++ JS/variables-for-numbers/app.js
@@ -0,0 +1,71 @@
+// Q1: You can assign a string to a variable. You can also assign a ______
+// ans: number
+
+// Q2: If a number is enclosed in quotes, it's a ______.
+// ans = string
+
+// Q3: What kind of operation can be done on a number variable that, in one particular case, can't be done on a string variable? Answer with a 4-letter word. (It would be a five-letter word in British usage.)
+// ans : math
+
+// Q4: What is the value of orderTotal?
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// Ans: 110
+
+// Q5: Drag-and-drop. Holding down the left mouse button, drag the pieces of code onto the screen to form a statement that declares a variable and assigns a value to it.
+// ans : var niceBigNumber = 1000000;
+
+// Q6: Click in the box and type the next character. I'll autocomplete. Don't type spaces.
+// In a single statement, declare the variable numberAsString and assign the number 3 to it as a string.
+// Ans: var  numberAsString = "3";
+
+// Q7: Fail-safe coding. If you type the wrong character, I'll cancel the keystroke. Type spaces.
+// Assign to the variable caseQty, which has already been declared, the value 144.
+// ans: caseQty = 144;
+
+// Q8: In a single statement declare a variable and assign a number to it. (You've learned only two rules for naming variables so far—no quotes and no initial number—so they're the only ones I'll hold you to.)
+// ans: var num = 3;
+
+// Q9: In a single statement declare a variable and assign to it the sum of 2 other variables. (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+// ans: var total = cost + profit;
+
+// Q10: Rewrite this statement so the variable can be used in a math operation.
+// var num = "9";
+// ans: var num = 9;
+
+// Q11: Assign the sum of 2 numbers to a variable, which hasn't been declared beforehand. (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+// ans: var sum = 1 + 1;
+
+// Q12: In one statement declare a variable. In a second statement assign it the sum of 2 numbers. (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+// ans: var total;
+//      total = 1 + 1;
+
+// Q13: In a single statement declare a variable and assign a number to it. You've learned only two rules for naming variables so far—no quotes and no initial number—so they're the only ones I'll hold you to. (The timer starts when you click in the answer field.)
+// ans : var num = 2;
+
+// Q14: In a single statement declare a variable and assign to it the sum of 2 other variables. (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+// ans: var total = cost + profit;
+
+// Q15: Rewrite this statement so the variable can be used in a math operation.
+// ans: var num = 9;
+
+// Q16: Assign the sum of 2 numbers to a variable, which hasn't been declared beforehand. (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+// ans: var total = 1 + 1;
+
+// Q17: In one statement declare a variable. In a second statement assign it the sum of 2 numbers. (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+// ans: num total;
+//      total = 1 + 1;
+
+// Q18: In the first statement declare a variable and assign it a number. In the second statement, change the value of the variable by adding it together with a number.
+// ans: var friends = 30;
+//      friends = friends + 10;
+
+
+
+
+
+
+
+
+
