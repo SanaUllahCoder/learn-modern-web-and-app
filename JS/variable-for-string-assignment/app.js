31a580e12f864ab879461c16b2dd1c91ec78f6e8
Sana Ullah Siddiqui <156604999+SanaUllahCoder@users.noreply.github.com>
2025-12-01 12:19:45 +0500

    complete assignment

---
+++ JS/variable-for-string-assignment/app.js
@@ -0,0 +1,79 @@
+
+// Q1: In the following code, "Mark" is a string. What is name (answer with 1 word)?
+
+var name = "Mark";
+// Ans: Variable
+
+// Q2: In one word, if it is enclosed in quotation marks, what is it?
+
+// Ans: String
+
+// Q3: What is the keyword that declares a variable?
+// Ans: var
+
+//Q4: This statement...
+// userName = "buddy5000";
+// ...is correct only if the variable has already been _______.
+// Ans: declared
+
+// Q5: Drag-and-drop. Holding down the left mouse button, drag the pieces of code onto the screen to form a statement that declares the variable punchline and assigns a value to it.
+// Ans: var punchline = "The accoutant";
+
+
+// Q6: Click in the box and type the next character. I'll autocomplete. Don't type spaces.
+
+// In a single statement, declare the variable nickname and assign the string "Satchmo" to it.
+
+// Ans: let nickname = "Stachmo";
+
+// Q7: Fail-safe coding. If you type the wrong character, I'll cancel the keystroke. Type spaces.
+// Declare the variable teamName.
+// Ans: var teamName;
+
+// Q8: In a single statement declare a variable and assign a string of your choice to it. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+// Ans: var yourChoice = "Grow";.
+
+// Q9: Declare a variable. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+// ans: var declare;
+
+// Q10:Assign a string of your choice to a variable of your choice that has already been declared. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+//  Ans: var firstName = "SanaUllah";
+
+// Q11: This statement has already been coded.
+// var bestMan = "Charlie";
+// Assign the variable a new string.
+// Ans: var bestman = "Charlie"
+
+// Q12: Declare a variable without defining it. Then, in a second statement, assign it a string. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+// Ans: var sanaUllah;
+//      sanaUllah = "Siddiqui";
+
+// Q13: Time goal: 30 seconds. (The timer starts when you click in the answer field.) In a single statement declare a variable of your choice and assign a string of your choice to it. You've learned only one rule for naming variables so far, so it's the only one I'll hold you to. (The timer starts when you click in the answer field.)
+// ans: var str = "String";
+
+// Q14: Declare a variable of your choice. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+// Ans: var declare;
+
+// Q15: Assign a string of your choice to a variable of your choice that has already been declared. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+// ans: var str = "String";
+
+// Q16: This statement has already been coded.
+// var bestMan = "Charlie";
+// Assign the variable a new string of your choice.
+// Ans: bestMan = "Charlie";
+
+// Q17: Declare a variable of your choice without defining it. Then, in a second statement, assign it a string of your choice. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+// Ans:  var game;
+//       game = "game";
+
+// Q18: In the first statement declare a variable of your choice and assign it a string of your choice. In the second statement change the value of the variable to a different string of your choice.
+// ans: var str = "string";
+// str = "game";
+
+// Q19: Declare a variable of your choice and assign it a string of your choice. Then code an alert, specifying the variable, not the string, as the message. Click the Run It button to run your code live. Type out the code, don't copy and paste it. If you copy and paste it from the screen showing the correct code, it may not work.
+// ans: var str = "string";
+    // alert = (Click the Run It button to run your code live. Type out the code, don't copy and paste it. If you copy and paste it from the screen showing the correct code, it may not work.);
+
+// // Q20: Declare a variable of your choice and assign it a string that's exactly the same as the variable name, but enclosed in quotation marks so JavaScript recognizes it as a string. Then code an alert, specifying the variable, not the string, as the message. Click the Run It button to run your code live. Type out the code, don't copy and paste it. If you copy and paste it from the screen showing the correct code, it may not work.
+// Ans: var crazyVariable = "crazyVariable";
+// alert(crazyVariable);
