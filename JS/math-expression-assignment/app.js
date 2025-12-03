bcb35cd14eedd8fbe90a5de28067aeec49eceb94
Sana Ullah Siddiqui <156604999+SanaUllahCoder@users.noreply.github.com>
2025-12-03 17:44:25 +0500

    complete math expression assignment

---
+++ JS/math-expression-assignment/app.js
@@ -0,0 +1,40 @@
+// Question 1
+var add = 2 + 2;
+document.write(add);
+Answer : 4
+
+// Question 2
+var subtract = 12 - 24;
+Answer : -12
+console.log(subtract);
+
+// Question 3 
+var multiply = 34 *4;
+Answer: 132
+console.log(multiply);
+
+// Question 4
+var num = 10;
+var popularNumber1 = num + 200;
+console.log(popularNumber1);
+Answer: 210
+
+// Question 5
+var num = 10;
+var anotherNum = 1;
+var popularNumber2 = num + anotherNum;
+Answer: 11
+console.log(popularNumber2);
+
+// Question 6
+var whatsLeftOver1 = 10 % 3;
+Answer: 1
+console.log(whatsLeftOver1);
+
+// Question 7
+var whatsLeftOver2 = 9 % 3;
+Answer : 0
+console.log(whatsLeftOver2);
+
+
+
