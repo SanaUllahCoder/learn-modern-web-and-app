b2e46ec578a113a680be415b0aa0a725b862fdad
Sana Ullah Siddiqui <156604999+SanaUllahCoder@users.noreply.github.com>
2025-12-10 12:51:57 +0000

    learn add a value start and end

---
+++ JS/array/app.js
@@ -0,0 +1,15 @@
+var fruits = ["banana", 'mango', 'apple', 'kiwi'];
+console.log('Before pop',fruits);
+
+fruits.pop();
+console.log('After pop', fruits)
+
+fruits.push('pear', 'kiwi', 'orange')
+console.log('After push 3 Fruits', fruits)
+
+var vegetables = ['cucumber', 'ladyfinder', 'potato']
+vegetables.shift();
+console.log('After shift', vegetables);
+
+vegetables.unshift()
+console.log('tomato', 'onion', vegetables)
