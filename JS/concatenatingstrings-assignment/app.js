71e25f2fbdc1681f2006da1ea6faaea0c0293fdd
Sana Ullah Siddiqui <sanaullah.siddiqui0317@gmail.com>
2025-12-17 21:31:51 +0500

    Completed concatenatingStrings Assignment

---
+++ JS/concatenatingstrings-assignment/app.js
@@ -0,0 +1,73 @@
+
+//  Q1: var num = "2" + "2";
+// What is the value of num? Include quotation marks.
+Answers: "22"
+
+// Q2: What is it called when you combine two or more strings, using the plus sign?
+Answer: concatenation
+
+
+// Q3: message = ("Hello," + "Dolly");
+// What is the value of message? (Include the quotation marks.)
+Answer: "Hello,Dolly"
+
+// Q4: What message displays in the alert box?
+alert("33" + 3);
+Answer: "333"
+
+// Q5: Drag-and-drop. Holding down the left mouse button, drag the pieces to create a statement that assigns to saying the concatenation of the two strings.
+// Answer: var saying = "upsy-" + "daisy";
+
+// Q6: Click in the box and type the next character. I'll autocomplete. Don't type spaces.
+
+// Write a statement that assigns to scribe the concatenation of the first and last names of England's greatest playwright. The variable hasn't been declared beforehand.
+// Answer: var scribe = "William" + "Shakespeare";
+
+// Q7: Fail-safe coding. If you type the wrong character, I'll cancel the keystroke. Type spaces.
+
+// Write an alert that displays the concatenation of the two parts of "Woo hoo"
+Answer: alert("Woo " + "hoo");
+
+// Q8: Write a statement that assigns to a variable the concatenation of the two parts of "Oh yeah" (no period at the end). The second part is "yeah" The variable hasn't been declared beforehand.
+// Answer: var phase = "Oh " + "yeah";
+
+// Q9: Write a statement that concatenates two variables and assigns the result to a third variable. The third variable hasn't been declared beforehand.
+// Answer:
+ var firstName = "Sana Ullah";
+var LastName = "Siddiqui";
+var fullName = firstName + lastName
+
+// Q10: Write a statement that displays an alert. The message is a string concatenated with a variable.
+// Answer: 
+var greeting = "Hello, ";
+alert(greeting + "Sana");
+
+// Q11: 
+var userName = "SanaUllahSiddiqui";
+alert("Thanks, " + userName + "!");
+// Answer: output "Thanks, SanaUllahSiddiqui!"
+
+Q12: 
+var message = "Thanks, ";
+var banger = "!";
+var userName = "SanaUllahSiddiqui";
+alert(message + userName + banger);
+// Answer: output "Thanks, SanaUllahSiddiqui!"
+
+// Q13: alert("Thanks, " + "Susan" + "!");
+// Answer: output "Thanks, Susan!"
+
+// Q14: 
+var message = "Thanks, ";
+var userName = "Susan";
+var banger = "!";
+var customMess = message + userName + banger;
+alert(customMess);
+// Answer: output "Thanks, Susan!"
+
+// Q15: alert("2" + "2");
+// Answer: output "22";
+
+// Q16: alert("2 plus 2 equals " + 2 + 2);
+// Answer: output "2 plus 2 equals 22"
+
