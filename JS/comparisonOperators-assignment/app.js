e2f55a6e6e226f3bdbbc36a6eab920c51aeabf75
Sana Ullah Siddiqui <sanaullah.siddiqui0317@gmail.com>
2025-12-17 22:12:24 +0500

    completed comparison operators assignment

---
+++ JS/comparisonOperators-assignment/app.js
@@ -0,0 +1,29 @@
+
+
+// Q1: ===, !==, >, and <= are ________ operators.
+// Answer: comparison
+
+// Q2: What is the comparison operator for is not equal to?
+// Answer: !==
+
+// Q3: What is the comparison operator for is greater than?
+// Answer: >
+
+// Q4: What is the comparison operator for is less than or equal to?
+// Answer: <=
+
+// Q5: Holding down the left mouse button, drag the pieces to create this if statement: If a isn't equal to b, a is assigned the value of c.
+Answer: 
+if(a !== b) {
+    a = c;
+}
+
+
+// Q6:  Click in the box and type the next character. I'll autocomplete. Don't type spaces.
+
+// Code the first line of an if statement that tests whether num1 is greater than num2.
+
+// Answer: if (num1 > num2) 
+
+// Q7: Code the first line of an if statement that tests whether num1 is greater than or equal to num2.
+// Answer: if (num1 >= num2) 
