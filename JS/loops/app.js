ebe1e0e46b4829a6dcc28e21467a1171e21616f6
Sana Ullah Siddiqui <156604999+SanaUllahCoder@users.noreply.github.com>
2025-12-17 12:50:44 +0000

    learn loops

---
+++ JS/loops/app.js
@@ -0,0 +1,33 @@
+
+// Loops
+
+// for loop
+
+for( var i = 0; i <= 10; i++){
+    document.write(i + ' hello' + '<br>')
+}
+
+for( var i = 10; i >= 0; i--){
+    document.write(i + ' hello' + '<br>')
+}
+
+var arr = ['ferri' , 'bugatti', 'lexus' , 'bently', 'landcruiser']
+
+for (var i = 0 ; i <= arr.length; i++){
+    document.write(arr[i] + '<br>')
+}
+
+for (var i = 1; i <= 10; i++){
+    document.write(2 + 'x' + i  + '=' +2 * i + '<br>')
+}
+
+var userInput = +prompt()
+var userresult = +prompt()
+
+for (var i = 1; i <= userresult; i++){
+    if (userInput && userresult < 1){
+    document.write(userInput + 'x' + i  + '=' +userInput * i + '<br>')
+    }else{
+        alert('please enter valid number')
+    }
+}
