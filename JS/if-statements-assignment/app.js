a09cf407cba669a8aca8415b48c7ec7a382d7239
Sana Ullah Siddiqui <sanaullah.siddiqui0317@gmail.com>
2025-12-17 21:59:03 +0500

    completed if statement assignment

---
+++ JS/if-statements-assignment/app.js
@@ -0,0 +1,32 @@
+
+// Q1:
+var x = prompt("Where does the Pope live?");
+if (x === "Vatican") {
+alert("Correct!");
+}else{
+alert("Wrong answer. The correct answer is Vatican.");
+}
+// Answer: If user inputs "Vatican", alert displays "Correct!", otherwise alert displays "Wrong answer. The correct answer is Vatican."
+
+
+// Q2: 
+var correctAnswer = "Vatican";
+if (x === correctAnswer) {
+alert("Correct!");
+}else {
+alert("Wrong answer. The correct answer is " + correctAnswer + ".");
+}
+
+// Answer: If user inputs "Vatican", alert displays "Correct!", otherwise alert displays "Wrong answer. The correct answer is Vatican."
+
+// Q3:
+ var correctAnswer = "Vatican";
+ if (x === correctAnswer) {
+    score++;
+    userIQ = "genius";
+    alert("Correct!");
+
+ }
+
+// Answer: If user inputs "Vatican", score is incremented by 1, userIQ is set to "genius", and alert displays "Correct!"
+
