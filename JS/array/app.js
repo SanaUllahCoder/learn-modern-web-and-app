736e14e35d6f7a51c84319ea091faa8975ceca9b
Sana Ullah Siddiqui <156604999+SanaUllahCoder@users.noreply.github.com>
2025-12-19 12:54:10 +0000

    learn logic

--- JS/array/app.js
+++ JS/array/app.js
@@ -1,15 +1,29 @@
-var fruits = ["banana", 'mango', 'apple', 'kiwi'];
-console.log('Before pop',fruits);
+// var fruits = ["banana", 'mango', 'apple', 'kiwi'];
+// console.log('Before pop',fruits);
 
-fruits.pop();
-console.log('After pop', fruits)
+// fruits.pop();
+// console.log('After pop', fruits)
 
-fruits.push('pear', 'kiwi', 'orange')
-console.log('After push 3 Fruits', fruits)
+// fruits.push('pear', 'kiwi', 'orange')
+// console.log('After push 3 Fruits', fruits)
 
-var vegetables = ['cucumber', 'ladyfinder', 'potato']
-vegetables.shift();
-console.log('After shift', vegetables);
+// var vegetables = ['cucumber', 'ladyfinder', 'potato']
+// vegetables.shift();
+// console.log('After shift', vegetables);
 
-vegetables.unshift()
-console.log('tomato', 'onion', vegetables)
+// vegetables.unshift()
+// console.log('tomato', 'onion', vegetables)
+
+var cities = ['Karachi', 'Islamabad', 'Lahore', 'Murre', 'Multan', 'Murree', 'Swat'];
+
+var userInput = prompt('enter city name');
+var isValid = false;
+
+for(var i = 0; 1 < cities.length; i++){
+    if(userInput.toLowerCase !== cities[i].toLowerCase){
+        alert('yes it is ');
+        isValid = true;
+    }else {
+        alert('no')
+    }
+}
--- JS/loops/app.js
+++ JS/loops/app.js
@@ -1,33 +1,20 @@
+var cities = ['Karachi', 'Swat', 'Lahore', 'Multan', 'Murree'];
 
-// Loops
+var userInput = prompt('enter city Name');
+var isValid = false;
 
-// for loop
 
-for( var i = 0; i <= 10; i++){
-    document.write(i + ' hello' + '<br>')
-}
-
-for( var i = 10; i >= 0; i--){
-    document.write(i + ' hello' + '<br>')
-}
-
-var arr = ['ferri' , 'bugatti', 'lexus' , 'bently', 'landcruiser']
-
-for (var i = 0 ; i <= arr.length; i++){
-    document.write(arr[i] + '<br>')
-}
-
-for (var i = 1; i <= 10; i++){
-    document.write(2 + 'x' + i  + '=' +2 * i + '<br>')
-}
-
-var userInput = +prompt()
-var userresult = +prompt()
-
-for (var i = 1; i <= userresult; i++){
-    if (userInput && userresult >= 1){
-    document.write(userInput + 'x' + i  + '=' +userInput * i + '<br>')
-    }else{
-        alert('please enter valid number')
+for ( var i =0; i < cities.length; i++){
+    if(userInput == cities[i]){
+        alert("you are eligible ");
+        isValid = true;
+        break;
     }
+};
+
+if(userInput != cities[i]){
+    alert('you are not eligible');
+    isValid = false;
 }
+
+console.log(isValid)
