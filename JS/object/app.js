96b97d2eecb75924c3959ac3aefa3a1c01e36c7a
Sana Ullah Siddiqui <156604999+SanaUllahCoder@users.noreply.github.com>
2025-12-31 12:58:58 +0000

    today learn object

---
+++ JS/object/app.js
@@ -0,0 +1,51 @@
+
+// var obj1 = {
+//     stdName : 'Sana Ullah',
+//     rollNum : 12,
+//     class : 'intermediate',
+//     age: 12,
+// }
+
+// // document.write(obj1.stdName, obj1.rollNum, obj1.class);
+// // document.write(obj1);
+// // console.log(obj1)
+
+// for(var key in obj1){
+//     document.write(key + ':' + obj1[key] + "<br>")
+// }
+
+// var obj
+
+var plan1 = {
+    name : "Basic" ,
+    price: 3.99,
+    space : 100,
+    transfer : 1000,
+    pages : 10,
+}
+
+var plan2 = {
+    name : "Professional" ,
+    price: 5.99,
+    space : 500,
+    transfer : 5000,
+    pages : 50,
+}
+
+var plan3 = {
+    name : "Ultimate" ,
+    price: 9.99,
+    space : 2000,
+    transfer : 20000,
+    pages : 500,
+}
+
+var name = document.getElementById("name").innerHTML= plan1.name;
+
+
+// var plan11= document.write(plan3.name, plan3.price +'<br>');
+// console.log(plan11)
+
+// document.write(plan2.space, plan2.pages + '<br>');
+
+// document.write(plan1.transfer, plan1.price +'<br>')
