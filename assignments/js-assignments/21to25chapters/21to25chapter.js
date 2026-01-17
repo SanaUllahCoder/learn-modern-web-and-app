00edde7275bacfa923075ec851c76effb9ebb4ca
Sana Ullah Siddiqui <sanaullah.siddiqui0317@gmail.com>
2026-01-18 02:24:31 +0500

    completed the 21to25 quiz

---
+++ assignments/js-assignments/21to25chapters/21to25chapter.js
@@ -0,0 +1,119 @@
+// Q1: Type the characters that are missing from this code.
+// var allLower = userInput.toLowerCase;
+// ans: ()
+
+// Q2: Type the character that is missing from this code.
+// var allLower = userInput toLowerCase();
+// ans: (.)
+
+// Q3: Two characters in this code are incorrect. Type the two correct characters.
+// var allLower = userInput.tolowercase();
+// ans: (C)(C);
+
+// Q4: What is the value of lowerVersion? Include the quotation marks in your answer.
+// var lowerVersion = "NO!".toLowerCase();
+
+// ans: "no!"
+
+// Q5: Convert the string represented by upperString to lower-case and assign the result to lowerString, which hasn't been declared beforehand.
+
+// ans: var lowerString = upperString.toLowerCase();
+
+// Q6: Convert the string represented by x to lower-case and assign the result to the same variable.
+
+// ans: x = x.toLowerCase();
+
+// Q7: Convert the string represented by a variable to lower-case and assign the result to a second variable that hasn't been declared beforehand.
+
+// ans: var nameLower = name.toLowerCase();
+
+
+// chapter 22
+
+// Q1: What is the method for extracting a segment from a string?
+// ans: slice()
+
+// Q2: captain" has been assigned to someWord. You want to slice "ap" out of it. Fill in the blank.
+// var segment = someWord.slice(___);
+// ans: 1, 3
+
+// Q3: You want to take a slice out of the string that starts at the fifth character and goes to the end. Fill in the blank.
+// var segment = someWord.slice(___);
+// ans: 4
+
+// Q4: The number of characters in the string will be assigned to the variable. Fill in the blank.
+// var numChars = someWord._______;
+// ans: length
+
+// Q5: The string "boxer" has been assigned to the array element athletes[3]. Slice "x" out of the string and assign it to the variable seg, which hasn't been declared beforehand.
+
+// ans: var seg = athletes[3].slice(2, 3);
+
+// chapter 23 
+// Q1: What is the keyword for finding the index of the first character of a segment?
+// ans: indexOf()
+
+
+// Q2: What is the keyword for finding the index of the first character of the last instance of a segment?
+// ans: lastIndexOf()
+
+// Q3: var text = "To be or not to be.";
+// var indx = text.indexOf("be");
+// What is the value of indx?
+// ans: 3
+
+// Q4: var text = "To be or not to be.";
+// var indx = text.lastIndexOf("be");
+// What is the value of indx?
+// ans : 16
+
+// Q5; Find the index of the first character of the last instance of "go" in the string represented by the variable text and assign the number to the variable indx, which hasn't been declared beforehand.
+// ans: var indx = text.lastIndexOf("go");
+
+// chapter 24
+// Q1: What is the keyword for finding a character at a particular location in a string?
+// ans: charAt()
+
+// Q2: In this string, what character is at index 2?—
+// "abcde"
+// Don't include quotation marks in your answer.
+// ans: c
+
+// Q3: Type the characters that are incorrect.
+// var cha = sentence.charAt[10];
+// ans: []
+
+// Q4: The last character of the string is assigned to the variable. Fill in the blank, using the string length to specify the index.
+// var lastChar = str.charAt(________);
+// ans: str.length - 1
+
+// Q5: Find the 10th character in the string represented by text and assign it to the variable cha, which hasn't been declared beforehand.
+
+// ans: var cha = text.charAt(9);
+
+// chapter 25
+
+// Q1: What is the keyword for removing one set of characters and inserting another set in its place?
+
+// ans: r
+
+// Q2:After these two statements execute, what is the value of x? Include quotation marks.
+// var x = "abc";
+// var y = x.replace("a", "z");
+// ans: "abc"
+
+
+// Q3:                    
+// If you want all instances replaced, enter 3 characters that need to appear in this statement.
+// var y = x.replace("a", "z");
+//ans : g
+
+// Q4: After these two statements execute, what is the value of x? Include quotation marks.
+// var x = "abc abc";
+// x = x.replace("a", "z");
+// ans: "zbc abc"
+
+
+// Q5: In the string represented by reply replace the first instance of "no" with "yes" and assign the revised string to revisedReply, which hasn't been declared beforehand.
+
+// ans: var revisedReply = reply.replace("no", "yes");
