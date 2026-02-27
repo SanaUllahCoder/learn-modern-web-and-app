667afeb46c598f6a5e1527b58d6f6a88e11b1a7f
Sana Ullah Siddiqui <156604999+SanaUllahCoder@users.noreply.github.com>
2026-02-27 11:50:49 +0000

    learn how to execute api

--- JS/ES6(ecmascript)/app.js
+++ JS/ES6(ecmascript)/app.js
@@ -56,11 +56,11 @@
 
 // CALLBACK FUNCTION
 
-function callBackFn(position){
-    console.log("Sana Ullah Siddiqui " + position )
+function callBackFn(position) {
+    console.log("Sana Ullah Siddiqui " + position)
 }
 
-function callBackFn1(callBack){
+function callBackFn1(callBack) {
     let position = "Frontend Developer";
     callBack(position)
 }
@@ -68,22 +68,77 @@ function callBackFn1(callBack){
 callBackFn1(callBackFn)
 
 // Example of callback function
-function sum(a, b, callback){
+function sum(a, b, callback) {
     return callback(a, b)
 }
-function add(a, b){
+function add(a, b) {
     return a + b;
 }
-console.log(sum(1,2, add))  
+console.log(sum(1, 2, add))
 
 // Example 2 of callback function
 let callBackExample = (name) => {
-   console.log(`Hello ${name}`);
+    console.log(`Hello ${name}`);
 }
 
 let callBackExamplefn = (callback) => {
-   let username = "Saylani";
-   callback(username)
+    let username = "Saylani";
+    callback(username)
 };
 
 callBackExamplefn(callBackExample);
+
+// PROMISE 
+
+let newPromise = new Promise((resolve, reject) => {
+    let username = true
+    if (username) {
+        console.log("promise chal raha he ")
+    } else {
+        console.log("abe bhai nahi chal raha he")
+    }
+})
+newPromise.then((sahi) => sahi)
+    .catch((bilkulsahi) => bilkulsahi)
+
+
+// FETCH
+
+let baseUrl = 'https://jsonplaceholder.typicode.com'
+let endPoint = {
+    posts : '/posts',
+    comment : '/comment',
+    todos : '/todos',
+    photos : '/photos'
+}
+
+let responseDummyApi = fetch(`${baseUrl}${endPoint.posts}`);
+responseDummyApi.then((data) => data.json())
+    .then((parse) => console.log(parse))
+    .catch((err) => console.log(err))
+
+
+// AYSNC / AWAIT 
+
+let async = async () => {
+    let response =  await fetch(`${baseUrl}${endPoint.todos}`);
+    let data = await response.json()
+    console.log(data);
+    let destructure = data.map((data) => data.userId )
+    console.log(destructure)
+    document.writeln(destructure);
+}
+async()
+
+
+
+// let asyncExample = async () => {
+//     let response =  await fetch('https://jsonplaceholder.typicode.com/posts');
+//     let data = await response.json()
+//     console.log(data);
+    
+//     let show = document.writeln(data)
+//     console.log()
+// }
+
+// asyncExample()
